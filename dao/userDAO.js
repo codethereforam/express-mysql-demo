13da544b3bcecf69e8f7cb62f75b4d6c9086beb8
thinkam <1203948298@qq.com>
2018-01-07 11:58:42 +0800

    完善添加用户的后台 & 修改目录结构

--- dao/userDAO.js
+++ dao/userDAO.js
@@ -3,19 +3,15 @@ var mysqlConf = require('../conf/mysqlConf');
 var dbUtils = require('../utils/dbUtils');
 var userSqlMap = require('./userSqlMap');
 var pool = mysql.createPool(dbUtils.extend({}, mysqlConf.mysql));
-var $result = require('../model/result');
 
 module.exports = {
-    add: function (user) {
-        var success = false;
+    add: function (user, callback) {
         pool.getConnection(function (err, connection) {
             connection.query(userSqlMap.add, [user.username, user.password], function (err, result) {
+                callback(result.affectedRows > 0);
                 connection.release();
-                // success =  result.affectedRows > 0;
             });
         });
-
-        return $result.createResult(success, "success");
     },
 
     delete: function () {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userDAO = require('../dao/userDAO');
+var $result = require('../model/result');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,8 +13,10 @@ router.post('/', function (req, res, next) {
     console.log('post users called');
     var user = req.body;
     console.log(user);
-    var result = userDAO.add(user);
-    res.json(result);
+    userDAO.add(user, function (success) {
+        var result =  $result.createResult(success, "success");
+        res.json(result);
+    });
 });
 
 module.exports = router;
