97c60edfc92018e2becdf63b7d1e40f5bb2e06af
codethereforam <1203948298@qq.com>
2018-01-16 14:33:49 +0800

    update 'user.sql' & 'users.js'

--- project-datamodel/user.sql
+++ project-datamodel/user.sql
@@ -1,4 +1,7 @@
-CREATE TABLE `learn-node`.`user` (
+create database `express-mysql-demo`;
+use `express-mysql-demo`;
+
+CREATE TABLE `express-mysql-demo`.`user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
