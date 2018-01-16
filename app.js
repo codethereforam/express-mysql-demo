8e076a021d41fbef75b802ea55903cf2cac1b5b6
codethereforam <1203948298@qq.com>
2018-01-16 15:05:55 +0800

    add website logo

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var users = require('./routes/users');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
